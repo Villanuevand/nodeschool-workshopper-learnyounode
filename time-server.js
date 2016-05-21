a91d78de24fd47c2007453162eddf23da1aa1cbc
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-21 17:50:12 -0300

    TIME-SERVER, resuelto

--- time-server.js
+++ time-server.js
@@ -1,3 +1,17 @@
-/**
- * Created by avillanueva on 21/5/16.
- */
+var net     = require('net');
+
+function fillWithZero (i) {
+    return (i < 10 ? '0' : '') + i
+}
+var server  = net.createServer(function (socket) {
+    var yy = new Date().getUTCFullYear(),
+        mm = fillWithZero(new Date().getMonth() + 1),
+        dd = fillWithZero(new Date().getDate()),
+        hh = new Date().getHours(),
+        min = new Date().getMinutes(),
+        data = yy + '-' + mm + '-' + dd + ' ' + hh + ':' + min + '\n';
+    console.log(new Date());
+    socket.end(data);
+});
+server.listen(process.argv[2]);
+
