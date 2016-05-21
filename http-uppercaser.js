289a0a9b95d750ddb3b46216aced5a3c11d1a63b
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-21 19:04:08 -0300

    HTTP UPPERCASE, resuelto

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -1,3 +1,14 @@
-/**
- * Created by avillanueva on 21/5/16.
- */
+var http = require('http');
+var map  = require('through2-map');
+
+
+var server = http.createServer(function (req, res) {
+    if (req.method == 'POST') {
+        req.pipe(map(function (chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(res);
+    }
+});
+
+server.listen(process.argv[2]);
+
