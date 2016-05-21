50950b237933c8e4c1d11d54f20391fefa90ac29
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-21 16:36:13 -0300

    JUGGLING-ASYNC, resuelto

---
+++ juggling-async.js
@@ -0,0 +1,35 @@
+var http = require('http');
+
+printUrl();
+
+function printUrl(){
+    var d = [],
+        c = 0,
+        url = process.argv;
+
+    for (var i = 2; i < url.length; i++) {
+        getText(url[i]);
+    }
+
+    printAll(d);
+
+    function getText (url) {
+        var daux = '';
+        http.get(url, function (response) {
+            response.setEncoding('utf8');
+            response.on('data', function (data) {
+                daux += data;
+            });
+            response.on('end', function () {
+                d.push(daux);
+                console.log(daux);
+            })
+        });
+    }
+
+    function printAll (arr){
+        for (var i = 0; i < arr.length; i++) {
+            console.log('print, ',d[i]);
+        }
+    }
+}
