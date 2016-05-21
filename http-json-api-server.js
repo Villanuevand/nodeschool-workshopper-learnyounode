87618e675d1d145012a8bd8202b58ed324b7277a
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-21 19:04:40 -0300

    HTTP JSON API SERVER, resuelto

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,3 +1,9 @@
-/**
- * Created by avillanueva on 21/5/16.
- */
+var http = require('http');
+var url  = require('url');
+
+var server = http.createServer(function (req, res) {
+    
+    res.send();
+    url.parse(req.url,true);
+});
+
