7280b7ee62771943b36e4d234682d810d63e38fd
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-22 21:44:07 -0300

    HTTP JSON API SERVER, resuelto..

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,9 +1,29 @@
-var http = require('http');
-var url  = require('url');
+var http = require("http");
+var url = require("url");
 
-var server = http.createServer(function (req, res) {
-    
-    res.send();
-    url.parse(req.url,true);
-});
+http.createServer(function (request, response) {
 
+    var dataSent = {};
+    var urlObj = url.parse(request.url);
+    var date = new Date(urlObj.query.replace("iso=", ""));
+
+    switch(urlObj.pathname) {
+        case "/api/parsetime":
+            dataSent = {
+                hour : date.getHours(),
+                minute : date.getMinutes(),
+                second : date.getSeconds()
+            };
+            break;
+
+        case "/api/unixtime":
+            dataSent = {
+                unixtime : date.getTime()
+            };
+            break;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(dataSent));
+
+}).listen(process.argv[2]);
